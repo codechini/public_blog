5560b7fa956b97306741c726e519b8bd569daa8f
codechini <arangathvenuchinmay@gmail.com>
2024-08-31 20:36:24 +0530

    gr layout

--- agaetis_blog/src/App.jsx
+++ agaetis_blog/src/App.jsx
@@ -6,9 +6,39 @@ function App() {
 
   return (
     <>
+      <nav className="top">
+        <header className='responsive'>
+          <nav>
+            <button className="small-round">Button</button>
+            <h5 className="max center-align">pForbes</h5>
+            <button className="border small-round">Button</button>
+          </nav>
+        </header>
+      </nav>
       <main className="responsive">
-        <h1 className="title">My Blog</h1>
-        <BlogPost />
+        <div className="hero">
+          <div className="hero-image-text">
+            <h1>This is a Title</h1>
+          </div>
+          {/* <img src='' alt="" /> */}
+        </div>
+        <div className="vertical small-space mobile">
+          <div className="s6">
+            <BlogPost />
+            <div className="space"></div>
+          </div>
+          <div className="s6">
+            <BlogPost />
+          </div>
+        </div>
+        <div className="grid desktop">
+          <div className="s6">
+            <BlogPost />
+          </div>
+          <div className="s6">
+            <BlogPost />
+          </div>
+        </div>
       </main>
 
     </>
